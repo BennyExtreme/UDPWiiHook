132bd6f92e87145ad5ea5e5e536d33ddef78c4fd
Benito Alessandrino <bennygames.yt@gmail.com>
2024-02-18 20:49:39 +0100

    Update
    - Add constant program name for config file
    - Check every config field and type of value
    - Remove unused variable while getting public address
    - Add option to change DSU port
    - Simplify config variable access
    - Bump version
    [build]

--- UDPWiiHook/Program.cs
+++ UDPWiiHook/Program.cs
@@ -9,7 +9,8 @@ namespace UDPWiiHook
 {
     internal static class Program
     {
-        public const string programVersion = "1.0.6";
+        public const string programName = "UDPWiiHook";
+        public const string programVersion = "1.0.7";
         public static IniFile config;
 
         public static void Main(string[] args)
@@ -17,28 +18,23 @@ namespace UDPWiiHook
             // Initialize the configuration file
             config = new IniFile();
 
-            // Check if config content exists, if not create it
-            if (!config.KeyExists("checkUpdates"))
-            {
-                config.Write("checkUpdates", "1");
-                config.Write("popupWindow", "1");
-            }
+            // Check if config content exists and if the content is valid, if not fix it
+            if (!config.KeyExists("port", programName) || !int.TryParse(config.Read("port", programName), out _)) config.Write("port", "26760", programName);  // "out _" -> "out int value"
+            if (!config.KeyExists("checkUpdates", programName) || !int.TryParse(config.Read("checkUpdates", programName), out _)) config.Write("checkUpdates", "1", programName);
+            if (!config.KeyExists("popupWindow", programName) || !int.TryParse(config.Read("popupWindow", programName), out _)) config.Write("popupWindow", "1", programName);
 
             for (int slotIndex = 0; slotIndex < 4; slotIndex++)
             {
-                if (!config.KeyExists("port", "Slot-" + slotIndex))
-                {
-                    config.Write("port", (4434 + slotIndex).ToString(), "Slot-" + slotIndex);
-                    config.Write("broadcastName", "UDPWiiHook@{id}", "Slot-" + slotIndex);
-                    config.Write("onlyLocalIps", "0", "Slot-" + slotIndex);
-                    config.Write("whitelist", "0", "Slot-" + slotIndex);
-                    config.Write("blacklist", "0", "Slot-" + slotIndex);
-                    config.Write("commaSeparatedIps", "", "Slot-" + slotIndex);
-                }
+                if (!config.KeyExists("port", "Slot-" + slotIndex) || !int.TryParse(config.Read("port", "Slot-" + slotIndex), out _)) config.Write("port", (4434 + slotIndex).ToString(), "Slot-" + slotIndex);
+                if (!config.KeyExists("broadcastName", "Slot-" + slotIndex)) config.Write("broadcastName", "UDPWiiHook@{id}", "Slot-" + slotIndex);
+                if (!config.KeyExists("onlyLocalIps", "Slot-" + slotIndex) || !int.TryParse(config.Read("onlyLocalIps", "Slot-" + slotIndex), out _)) config.Write("onlyLocalIps", "0", "Slot-" + slotIndex);
+                if (!config.KeyExists("whitelist", "Slot-" + slotIndex) || !int.TryParse(config.Read("whitelist", "Slot-" + slotIndex), out _)) config.Write("whitelist", "0", "Slot-" + slotIndex);
+                if (!config.KeyExists("blacklist", "Slot-" + slotIndex) || !int.TryParse(config.Read("blacklist", "Slot-" + slotIndex), out _)) config.Write("blacklist", "0", "Slot-" + slotIndex);
+                if (!config.KeyExists("commaSeparatedIps", "Slot-" + slotIndex)) config.Write("commaSeparatedIps", "", "Slot-" + slotIndex);
             }
 
             // Check for updates
-            if (int.Parse(config.Read("checkUpdates")) == 1)
+            if (int.Parse(config.Read("checkUpdates", programName)) == 1)
             {
                 try
                 {
@@ -51,7 +47,7 @@ namespace UDPWiiHook
                         if (latestVersion != programVersion)
                         {
                             Console.WriteLine("--- --- ---\nA new version of the program is available for download!\n\nLatest Version: {0}\nCurrent Version: {1}\n\nYou can disable version check in the .ini config file.\n--- --- ---", latestVersion, programVersion);
-                            if (int.Parse(config.Read("popupWindow")) == 1)
+                            if (int.Parse(config.Read("popupWindow", programName)) == 1)
                             {
                                 DialogResult result = MessageBox.Show(String.Format("A new version of the program is available for download,\nclick \"OK\" to open the download website.\n\nLatest Version: {0}\nCurrent Version: {1}\n\nYou can disable version check in the .ini config file.", latestVersion, programVersion), "New version available", MessageBoxButtons.OKCancel, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
 
@@ -81,13 +77,13 @@ namespace UDPWiiHook
 
             try
             {
-                DSU.Server.CreateInstance(id, 26760);
+                DSU.Server.CreateInstance(id, ushort.Parse(config.Read("port", programName)));
                 UDPWii.Server.servers = new UDPWii.Server[]
                 {
-                    new UDPWii.Server(id, 0, ushort.Parse(Program.config.Read("port", "Slot-" + 0))),
-                    new UDPWii.Server(id, 1, ushort.Parse(Program.config.Read("port", "Slot-" + 1))),
-                    new UDPWii.Server(id, 2, ushort.Parse(Program.config.Read("port", "Slot-" + 2))),
-                    new UDPWii.Server(id, 3, ushort.Parse(Program.config.Read("port", "Slot-" + 3)))
+                    new UDPWii.Server(id, 0, ushort.Parse(config.Read("port", "Slot-" + 0))),
+                    new UDPWii.Server(id, 1, ushort.Parse(config.Read("port", "Slot-" + 1))),
+                    new UDPWii.Server(id, 2, ushort.Parse(config.Read("port", "Slot-" + 2))),
+                    new UDPWii.Server(id, 3, ushort.Parse(config.Read("port", "Slot-" + 3)))
                 };
 
                 Console.WriteLine("[Program] Possible local addresses:");
@@ -105,7 +101,7 @@ namespace UDPWiiHook
                     Console.WriteLine("\t" + publicIpAddress);
                     Console.WriteLine("Remember to open UDP slots ports in your router if you want to use clients remotely");
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     Console.WriteLine("[Program] Failed to retrieve public IP address (not connected to internet?)");
                 }
